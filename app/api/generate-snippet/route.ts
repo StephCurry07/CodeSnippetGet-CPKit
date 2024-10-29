c41bf30be8e57b780b5eae5e7815abf9c5e122a4
Apoorv Gupta <apoorv.gupta65@gmail.com>
2024-10-29 15:34:08 +0530

    fixed imports and comps

--- app/api/generate-snippet/route.ts
+++ app/api/generate-snippet/route.ts
@@ -3,11 +3,11 @@ import {
   GroqAdapter,
   copilotRuntimeNextJSAppRouterEndpoint
 } from '@copilotkit/runtime';
-import Groq from 'groq-sdk';
+// import Groq from 'groq-sdk';
 import { NextResponse } from 'next/server';
    
 //   const groqApiKey = process.env.GROQ_API_KEY; 
-  const groq = new Groq({ apiKey: process.env.GROQ_API_KEY});
+  // const groq = new Groq({ apiKey: process.env.GROQ_API_KEY});
   // const serviceAdapter = new OpenAIAdapter({ openai, model:'gpt-3.5-turbo' });
 
   const serviceAdapter = new GroqAdapter({ model: "llama3-8b-8192" }); 
